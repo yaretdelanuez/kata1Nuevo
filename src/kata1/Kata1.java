ffdddb63f8bb6a55ff8110b244058b368300a7aa
Yar <Yar@DESKTOP-Yaret>
2020-10-18 18:22:53 +0100

    kata1 versión 3

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,16 +1,19 @@
 
 package kata1;
 
-import java.util.Date;
+import java.time.LocalDate;
+
 
 
 public class Kata1 {
 
   
     public static void main(String[] args) {
-        Person person = new Person("Blas", new Date(49,8,24));
-        System.out.println(person.getName() + "tiene "+ person.getAge());
-        //
+        LocalDate date = LocalDate.of(1999, 12, 6);        
+        
+        Person person = new Person("Yaret", date);
+        System.out.println(person.getName() + " tiene "+ person.getAge() + "años");
+        
     }
     
 }
