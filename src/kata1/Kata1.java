75edf2b16064d38567acca4152f0d75fd81baf1e
Yar <Yar@DESKTOP-Yaret>
2020-10-18 17:38:45 +0100

    primera version kata1

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -10,6 +10,7 @@ public class Kata1 {
     public static void main(String[] args) {
         Person person = new Person("Blas", new Date(49,8,24));
         System.out.println(person.getName() + "tiene "+ person.getAge());
+        //
     }
     
 }
